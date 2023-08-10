ec32e951f543a117f7fc9a0b6883e9287da8ff30
CatsJuice <wp1298554944@hotmail.com>
2023-08-10 19:02:45 +0800

    feat(template/react): streamline code

--- templates/react/src/main.jsx
+++ templates/react/src/main.jsx
@@ -1,7 +1,3 @@
-function MyApp() {
-  return <h1>Hello, world!</h1>;
-}
+const App = () => <h1>Hello, world!</h1>
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<MyApp />);
+ReactDOM.createRoot(root).render(<App />);
